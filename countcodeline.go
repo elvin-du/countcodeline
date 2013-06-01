6d58e8b607f6fab8ffa27c738eea94e8c72066b0
dumx <dumx@dumx-ThinkPad-SL410.(none)>
2013-06-01 13:46:59 +0800

    add a function of parsing config file

--- countcodeline.go
+++ countcodeline.go
@@ -2,25 +2,51 @@ package main
 
 import(
 	"os"
+	"io"
 	"log"
-	"os/exec"
 	"path/filepath"
+	"bufio"
 )
 
 func init(){
 
 }
 
+
 func main(){
 	fullPath := GetSrcFullPath()
+
+	ParseConf()
 	log.Println(fullPath)
+}
+
+func ParseConf()(conf []string, err error){
+	confPath,_ := os.Getwd()
+	confPath +=  "/conf"
+	file,err := os.Open(confPath)
+	if nil != err{
+		log.Println(err)
+		return
+	}
+	defer file.Close()
+
+	rd := bufio.NewReader(file)
+	var line string
+	for{
+		line, err = rd.ReadString('\n')
+		if io.EOF == err || nil != err{
+			break
+		}
+		conf = append(conf, line)
+	}
+	return
 }
 
 func GetSrcFullPath()(fullPath string){
 	args := os.Args;
 	parameterLen := len(args)
 	if parameterLen == 1{
-		fullPath,_ = exec.LookPath(args[0])
+		fullPath,_ = os.Getwd()
 	}else if(parameterLen >= 2){
 		fullPath = args[1]
 	}
